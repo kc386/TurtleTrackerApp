8d9bab8366c8622060f1143692d8d78388289032
kc386 <69813047+kc386@users.noreply.github.com>
2020-09-20 12:20:29 -0400

    Initial commit of ARGOS script and VMOpenspyder.bat file

---
+++ OpenSpyderVM.bat
@@ -0,0 +1,4 @@
+@set the_env=env859-py3
+call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "%the_env%"
+call "C:\Users\kc386\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\env859-py3\Scripts\spyder.exe" -p %cd% 
+call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
